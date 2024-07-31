45b7907c1947847a4f048ced7f71c2b2eb3cd31b
shiv2004-apexunit <22103008@mail.jiit.ac.in>
2024-07-31 23:07:34 +0530

    Update code1.cpp

--- code1.cpp
+++ code1.cpp
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main() {
+  int n;
+  printf("Enter size: ");
+  scanf("%d", &n);
+  int arr[n];
+  int sum = 0;
+  printf("Enter %d elements:\n", n);
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &arr[i]);
+    sum += arr[i];
+  }
+
+
+  float average = (float)sum / n;
+  printf("Average: %.2f\n", average);
+
+  return 0;
+}
